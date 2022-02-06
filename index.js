25595fc93de3fecb5fcd4180cc90683865dff049
shazwi <supershazwi@gmail.com>
2022-02-06 20:54:49 +0800

    completed signup, login and logout

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import methodOverride from "method-override";
 import cookieParser from "cookie-parser";
 import pg from "pg";
 import jsSHA from 'jssha';
+import e from "express";
 
 const app = express();
 const { Pool } = pg;
@@ -14,12 +15,59 @@ const pgConnectionConfigs = {
 };
 
 const pool = new Pool(pgConnectionConfigs);
+const SALT = 'bananas are delicious';
 
 app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
+
+app.get('/note', (request, response) => {
+  if (request.cookies.userId !== undefined) {
+    const shaObj = new jsSHA('SHA-512', 'TEXT', {encoding: 'UTF8'});
+    const unhashedCookieString = `${request.cookies.userId}-${SALT}`;
+    shaObj.update(unhashedCookieString);
+    const hashedCookieString = shaObj.getHash('HEX');
+
+    if (hashedCookieString === request.cookies.loggedInHash) {
+      const data = {
+        userId: request.cookies.userId
+      }
+      response.render('admin', data);
+    } else {
+      response.redirect('/login');
+    }
+  } else {
+    response.redirect('/login');
+  }
+});
+
+app.get('/admin', (request, response) => {
+  if (request.cookies.userId !== undefined) {
+    const shaObj = new jsSHA('SHA-512', 'TEXT', {encoding: 'UTF8'});
+    const unhashedCookieString = `${request.cookies.userId}-${SALT}`;
+    shaObj.update(unhashedCookieString);
+    const hashedCookieString = shaObj.getHash('HEX');
+
+    if (hashedCookieString === request.cookies.loggedInHash) {
+      const data = {
+        userId: request.cookies.userId
+      }
+      response.render('admin', data);
+    } else {
+      response.redirect('/login');
+    }
+  } else {
+    response.redirect('/login');
+  }
+})
+
+app.delete('/logout', (request, response) => {
+  response.clearCookie("userId");
+  response.clearCookie("loggedInHash");
+  response.redirect('/login');
+});
 
 app.post("/signup", (request, response) => {
   const { email, password } = request.body;
@@ -36,7 +84,6 @@ app.post("/signup", (request, response) => {
       return;
     }
 
-    console.log("User added");
     response.render("signup");
   });
 });
@@ -48,7 +95,11 @@ app.get("/signup", (request, response) => {
 app.post("/login", (request, response) => {
   const { email, password } = request.body;
 
-  const sqlQuery = `SELECT COUNT(*) FROM users WHERE email = '${email}' AND password = '${password}'`;
+  const shaObj = new jsSHA('SHA-512', 'TEXT', {encoding: 'UTF8'});
+  shaObj.update(password);
+  const hashedPassword = shaObj.getHash('HEX');
+
+  const sqlQuery = `SELECT * FROM users WHERE email = '${email}' AND password = '${hashedPassword}'`;
 
   pool.query(sqlQuery, (error, result) => {
     if (error) {
@@ -56,17 +107,32 @@ app.post("/login", (request, response) => {
       return;
     }
 
-    if (result.rowCount === 1) {
-      console.log("User found");
+    if (result.rows[0] !== undefined) {
+      const shaObj2 = new jsSHA('SHA-512', 'TEXT', {encoding: 'UTF8'});
+      const user = result.rows[0];
+      const unhashedCookieString = `${user.id}-${SALT}`;
+      shaObj2.update(unhashedCookieString);
+      const hashedCookieString = shaObj2.getHash('HEX');
+
+      response.cookie('loggedInHash', hashedCookieString);
+      response.cookie('userId', user.id);
+      response.redirect("/admin");
+
     } else {
-      console.log("User not found");
+      response.redirect("/login");
     }
-    response.render("login");
   });
 });
 
 app.get("/login", (request, response) => {
-  response.render("login");
+  if (request.cookies.userId !== undefined) {
+    response.redirect('/admin');
+  } else {
+    const data = {
+      userId: request.cookies.userId
+    }
+    response.render("login", data);
+  }
 });
 
 app.listen(3004);
